5083164d340cb601576c97283b82b4c04f59f7bf
quluzadeasime <161600354+quluzadeasime@users.noreply.github.com>
2024-06-15 09:10:27 +0400

    front to back completed

---
+++ TREKER/TREKER/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+namespace TREKER.Controllers
+{
+    public class HomeController : Controller
+    {
+       public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Destinations()
+        {
+            return View();
+        }
+        public IActionResult Trekking()
+        {
+            return View();
+        }
+        public IActionResult News()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+      
+    }
+}
